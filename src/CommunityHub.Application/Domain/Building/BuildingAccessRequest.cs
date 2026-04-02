1d5f76404e84f660cf9a3aa110240395750351f3
Katarina Ostojic <ostojickatarinaaaa@gmail.com>
2026-04-02 13:46:21 +0200

    add database shema and domain model

---
+++ src/CommunityHub.Application/Domain/Building/BuildingAccessRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace CommunityHub.Application.Domain.Building;
+
+public class BuildingAccessRequest
+{
+    public long Id { get; private set; }
+    public User User { get; private set; }
+    public Building Building { get; private set; }
+    public string UnitNumber { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public string Status { get; private set; }
+    public string? RejectionReason { get; private set; }
+
+    public BuildingAccessRequest(long id, User user, Building building, string unitNumber, DateTime createdAt, string status, string? rejectionReason = null)
+    {
+        Id = id;
+        User = user;
+        Building = building;
+        UnitNumber = unitNumber;
+        CreatedAt = createdAt;
+        Status = status;
+        RejectionReason = rejectionReason;
+    }
+}
