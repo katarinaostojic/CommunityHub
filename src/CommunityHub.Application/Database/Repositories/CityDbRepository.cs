a1d9c450163e2bb4b69529065a1b500bcbd23df7
nenadveselinovic4 <nenadveselinovic89@gmail.com>
2026-04-14 16:27:28 +0200

    refactor cities and countries selection

--- src/CommunityHub.Application/Database/Repositories/CityDbRepository.cs
+++ src/CommunityHub.Application/Database/Repositories/CityDbRepository.cs
@@ -106,4 +106,31 @@ public class CityDbRepository : BaseDbRepository
 
         command.ExecuteNonQuery();
     }
+
+    public List<City> GetByCountry(long countryId)
+    {
+        using IDbConnection connection = PostgresConnection.CreateConnection();
+        IDbCommand command = connection.CreateCommand();
+        command.CommandText = @"
+        SELECT ci.id, ci.name, co.id as country_id, co.name as country_name, co.code as country_code
+        FROM cities ci
+        JOIN countries co ON ci.country_id = co.id
+        WHERE ci.country_id = @countryId
+        ORDER BY ci.name";
+
+        AddParameter(command, "@countryId", countryId);
+
+        using IDataReader reader = command.ExecuteReader();
+        List<City> cities = new List<City>();
+        while (reader.Read())
+        {
+            Country country = new Country(
+                Convert.ToInt64(reader["country_id"]),
+                reader["country_name"].ToString()!,
+                reader["country_code"].ToString()!
+            );
+            cities.Add(new City(Convert.ToInt64(reader["id"]), reader["name"].ToString()!, country));
+        }
+        return cities;
+    }
 }
