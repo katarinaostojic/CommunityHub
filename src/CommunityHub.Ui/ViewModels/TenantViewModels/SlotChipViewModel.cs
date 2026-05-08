b2d86191bc2037336b2fe5fbc2715af04d214a35
Katarina Ostojic <ostojickatarinaaaa@gmail.com>
2026-05-08 13:52:21 +0200

    add ViewModels for AdDetailsPage

---
+++ src/CommunityHub.Ui/ViewModels/TenantViewModels/SlotChipViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityHub.Application.Domain.Ads;
+
+namespace CommunityHub.Ui.ViewModels.TenantViewModels;
+
+public class SlotChipViewModel : BaseViewModel
+{
+    private readonly AdSlot _slot;
+
+    public SlotChipViewModel(AdSlot slot)
+    {
+        _slot = slot;
+    }
+
+    public string Display => $"{_slot.StartTime:HH:mm} - {_slot.EndTime:HH:mm}";
+}
