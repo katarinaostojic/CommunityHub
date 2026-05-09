ea93f06b3f0aa65f669d470ed666e3c18af33952
Katarina Ostojic <ostojickatarinaaaa@gmail.com>
2026-05-09 19:43:58 +0200

    add selectable slot view models for booking flow

---
+++ src/CommunityHub.Ui/ViewModels/TenantViewModels/Ads/SelectableFreeSlotDayGroupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityHub.Application.Domain.Ads;
+
+namespace CommunityHub.Ui.ViewModels.TenantViewModels.Ads;
+
+public class SelectableFreeSlotDayGroupViewModel : BaseViewModel
+{
+    public SelectableFreeSlotDayGroupViewModel(DateOnly date, List<AdSlot> slots)
+    {
+        DateDisplay = date.ToString("dd.MM");
+        Slots = slots.Select(s => new SelectableSlotChipViewModel(s)).ToList();
+    }
+
+    public string DateDisplay { get; }
+    public List<SelectableSlotChipViewModel> Slots { get; }
+}
