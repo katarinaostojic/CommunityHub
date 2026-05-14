83972182b18a5582ec838ca36f3b4c5bcc867fc5
stanoje31 <stanojevicn31@gmail.com>
2026-05-14 09:44:09 +0200

    Feature: add domain, repository and service for citizen 4.3 events

---
+++ src/CommunityHub.Application/Domain/Neighborhoods/NeighborhoodRepositoryInterfaces/IEventRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CommunityHub.Application.Domain.Neighborhoods;
+
+namespace CommunityHub.Application.Domain.Neighborhoods.NeighborhoodRepositoryInterfaces;
+
+public interface IEventRepository
+{
+    long Create(Event ev);
+    List<Event> GetByNeighborhood(long neighborhoodId);
+    Event? GetById(long eventId);
+    void Update(Event ev);
+    void AddRegistration(long eventId, long citizenId, List<long> itemIds);
+}
