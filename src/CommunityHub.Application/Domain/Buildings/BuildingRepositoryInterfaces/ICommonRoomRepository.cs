b0ce70357315c875532c454e48f2dec074aca267
nenadveselinovic4 <nenadveselinovic89@gmail.com>
2026-05-15 21:15:44 +0200

    feat: add CommonRoomRequest domain class and repository interface

--- src/CommunityHub.Application/Domain/Buildings/BuildingRepositoryInterfaces/ICommonRoomRepository.cs
+++ src/CommunityHub.Application/Domain/Buildings/BuildingRepositoryInterfaces/ICommonRoomRepository.cs
@@ -6,4 +6,5 @@ public interface ICommonRoomRepository
     long Create(string name, string description, int floorNumber, string rentalType, long buildingId);
     List<DateTime> GetOccupiedDates(long roomId);
     CommonRoom? GetById(long roomId);
+    void BookDate(long commonRoomId, DateTime date);
 }
