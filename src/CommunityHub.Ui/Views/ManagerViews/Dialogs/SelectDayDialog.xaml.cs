333666e27966069d1398162e127bc236fdf843fb
nenadveselinovic4 <nenadveselinovic89@gmail.com>
2026-05-15 21:18:17 +0200

    feat: add CommonRoomRequest UI and calendar

---
+++ src/CommunityHub.Ui/Views/ManagerViews/Dialogs/SelectDayDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CommunityHub.Ui.ViewModels.ManagerViewModels.Buildings;
+using System.Windows;
+
+namespace CommunityHub.Ui.Views.ManagerViews.Dialogs;
+
+public partial class SelectDayDialog : Window
+{
+    private readonly long _requestId;
+    private readonly CommonRoomRequestViewModel _viewModel;
+
+    public SelectDayDialog(List<DateTime> freeDays, long requestId,
+                           CommonRoomRequestViewModel viewModel)
+    {
+        InitializeComponent();
+        _requestId = requestId;
+        _viewModel = viewModel;
+        DaysListBox.ItemsSource = freeDays;
+    }
+
+    private void ApproveButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (DaysListBox.SelectedItem is DateTime selectedDay)
+        {
+            _viewModel.ApproveWithDate(_requestId, selectedDay);
+            DialogResult = true;
+            Close();
+        }
+        else
+        {
+            MessageBox.Show("Please select a day.", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+}
