fbeb8e1ee3bac99de229750e9d005411b41fb176
Katarina Ostojic <ostojickatarinaaaa@gmail.com>
2026-05-19 11:58:45 +0200

    Extract ad slot mapping

---
+++ src/CommunityHub.Application/Database/Mappers/Ads/AdSlotMapper.cs
@@ -0,0 +1,43 @@
+﻿using CommunityHub.Application.Domain;
+using CommunityHub.Application.Domain.Ads;
+using System.Data;
+
+namespace CommunityHub.Application.Database.Mappers.Ads;
+
+public static class AdSlotMapper
+{
+    public static List<AdSlot> ReadSlots(IDataReader reader)
+    {
+        List<AdSlot> slots = new();
+
+        while (reader.Read())
+            slots.Add(AdMapper.MapSlot(reader));
+
+        return slots;
+    }
+
+    public static List<(AdSlot slot, Ad? bookedByAd)> ReadBookedSlotsWithAds(IDataReader reader)
+    {
+        List<(AdSlot, Ad?)> results = new();
+
+        while (reader.Read())
+            results.Add(MapBookedSlotWithAd(reader));
+
+        return results;
+    }
+
+    private static (AdSlot slot, Ad? bookedByAd) MapBookedSlotWithAd(IDataReader reader)
+    {
+        AdSlot slot = AdMapper.MapSlot(reader);
+        Ad? bookedByAd = HasBookedByAd(reader)
+            ? AdMapper.MapBookedByAd(reader, UserMapper.Map(reader))
+            : null;
+
+        return (slot, bookedByAd);
+    }
+
+    private static bool HasBookedByAd(IDataReader reader)
+    {
+        return !reader.IsDBNull(reader.GetOrdinal("ba_id"));
+    }
+}
