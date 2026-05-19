5df457f66e81dafa56c12ff596ce4dd6b2ad6841
Katarina Ostojic <ostojickatarinaaaa@gmail.com>
2026-05-19 13:30:59 +0200

    Clean up common room request repository mapping

--- src/CommunityHub.Application/Database/Repositories/Buildings/CommonRooms/CommonRoomRequestDbRepository.cs
+++ src/CommunityHub.Application/Database/Repositories/Buildings/CommonRooms/CommonRoomRequestDbRepository.cs
@@ -9,10 +9,20 @@ namespace CommunityHub.Application.Database.Repositories.Buildings.CommonRooms;
 
 public class CommonRoomRequestDbRepository : BaseDbRepository, ICommonRoomRequestRepository
 {
+    private static readonly UserColumnAliases TenantAliases = new(
+        "user_id",
+        "tenant_username",
+        "tenant_password",
+        "tenant_name",
+        "tenant_surname",
+        "tenant_birthday",
+        "tenant_role");
+
     public List<CommonRoomRequest> GetAllByCommonRoom(long commonRoomId)
     {
         using IDbConnection connection = PostgresConnection.CreateConnection();
         IDbCommand command = connection.CreateCommand();
+
         command.CommandText = @"
         SELECT r.id, r.date_from, r.date_to, r.status, 
                r.approved_date, r.proposed_date_from, r.proposed_date_to,
@@ -38,6 +48,7 @@ public class CommonRoomRequestDbRepository : BaseDbRepository, ICommonRoomReques
     {
         using IDbConnection connection = PostgresConnection.CreateConnection();
         IDbCommand command = connection.CreateCommand();
+
         command.CommandText = @"
         SELECT r.id, r.date_from, r.date_to, r.status,
                r.approved_date, r.proposed_date_from, r.proposed_date_to,
@@ -55,59 +66,28 @@ public class CommonRoomRequestDbRepository : BaseDbRepository, ICommonRoomReques
         AddParameter(command, "@requestId", requestId);
 
         using IDataReader reader = command.ExecuteReader();
-
-        if (!reader.Read())
-        {
-            return null;
-        }
-
-        return ReadRequest(reader);
-    }
-
-    public void Update(CommonRoomRequest request)
-    {
-        using IDbConnection connection = PostgresConnection.CreateConnection();
-        IDbCommand command = connection.CreateCommand();
-
-        command.CommandText = @"
-        UPDATE common_room_requests
-        SET date_from = @dateFrom,
-            date_to = @dateTo,
-            status = @status::common_room_request_status,
-            approved_date = @approvedDate,
-            proposed_date_from = @proposedDateFrom,
-            proposed_date_to = @proposedDateTo
-        WHERE id = @id";
-
-        AddParameter(command, "@id", request.Id);
-        AddParameter(command, "@dateFrom", request.DateFrom);
-        AddParameter(command, "@dateTo", request.DateTo);
-        AddParameter(command, "@status", CommonRoomRequestStatusMapper.ToDatabaseValue(request.Status));
-        AddParameter(command, "@approvedDate", request.ApprovedDate);
-        AddParameter(command, "@proposedDateFrom", request.ProposedDateFrom);
-        AddParameter(command, "@proposedDateTo", request.ProposedDateTo);
-
-        command.ExecuteNonQuery();
+        return ReadSingleRequest(reader);
     }
 
     public List<CommonRoomRequest> GetByTenant(long tenantId)
     {
         using IDbConnection connection = PostgresConnection.CreateConnection();
         IDbCommand command = connection.CreateCommand();
+
         command.CommandText = @"
-            SELECT r.id, r.date_from, r.date_to, r.status,
-                   r.approved_date, r.proposed_date_from, r.proposed_date_to,
-                   cr.id AS cr_id, cr.name, cr.description, cr.floor_number,
-                   cr.rental_type, cr.building_id,
-                   u.id AS user_id, u.username AS tenant_username,
-                   u.password AS tenant_password,
-                   u.name AS tenant_name, u.surname AS tenant_surname,
-                   u.birthday AS tenant_birthday, u.role AS tenant_role
-            FROM common_room_requests r
-            JOIN common_rooms cr ON r.common_room_id = cr.id
-            JOIN users u ON r.tenant_id = u.id
-            WHERE r.tenant_id = @tenantId
-            ORDER BY r.id DESC";
+        SELECT r.id, r.date_from, r.date_to, r.status,
+               r.approved_date, r.proposed_date_from, r.proposed_date_to,
+               cr.id AS cr_id, cr.name, cr.description, cr.floor_number,
+               cr.rental_type, cr.building_id,
+               u.id AS user_id, u.username AS tenant_username,
+               u.password AS tenant_password,
+               u.name AS tenant_name, u.surname AS tenant_surname,
+               u.birthday AS tenant_birthday, u.role AS tenant_role
+        FROM common_room_requests r
+        JOIN common_rooms cr ON r.common_room_id = cr.id
+        JOIN users u ON r.tenant_id = u.id
+        WHERE r.tenant_id = @tenantId
+        ORDER BY r.id DESC";
 
         AddParameter(command, "@tenantId", tenantId);
 
@@ -119,6 +99,7 @@ public class CommonRoomRequestDbRepository : BaseDbRepository, ICommonRoomReques
     {
         using IDbConnection connection = PostgresConnection.CreateConnection();
         IDbCommand command = connection.CreateCommand();
+
         command.CommandText = @"
         SELECT r.id, r.date_from, r.date_to, r.status,
                r.approved_date, r.proposed_date_from, r.proposed_date_to,
@@ -162,13 +143,51 @@ public class CommonRoomRequestDbRepository : BaseDbRepository, ICommonRoomReques
         return Convert.ToInt64(command.ExecuteScalar());
     }
 
+    public void Update(CommonRoomRequest request)
+    {
+        using IDbConnection connection = PostgresConnection.CreateConnection();
+        IDbCommand command = connection.CreateCommand();
+
+        command.CommandText = @"
+        UPDATE common_room_requests
+        SET date_from = @dateFrom,
+            date_to = @dateTo,
+            status = @status::common_room_request_status,
+            approved_date = @approvedDate,
+            proposed_date_from = @proposedDateFrom,
+            proposed_date_to = @proposedDateTo
+        WHERE id = @id";
+
+        AddParameter(command, "@id", request.Id);
+        AddParameter(command, "@dateFrom", request.DateFrom);
+        AddParameter(command, "@dateTo", request.DateTo);
+        AddParameter(command, "@status", CommonRoomRequestStatusMapper.ToDatabaseValue(request.Status));
+        AddParameter(command, "@approvedDate", request.ApprovedDate);
+        AddParameter(command, "@proposedDateFrom", request.ProposedDateFrom);
+        AddParameter(command, "@proposedDateTo", request.ProposedDateTo);
+
+        command.ExecuteNonQuery();
+    }
+
     public void Delete(long requestId)
     {
         using IDbConnection connection = PostgresConnection.CreateConnection();
         IDbCommand command = connection.CreateCommand();
+
         command.CommandText = "DELETE FROM common_room_requests WHERE id = @id";
+
         AddParameter(command, "@id", requestId);
         command.ExecuteNonQuery();
+    }
+
+    private static CommonRoomRequest? ReadSingleRequest(IDataReader reader)
+    {
+        if (!reader.Read())
+        {
+            return null;
+        }
+
+        return ReadRequest(reader);
     }
 
     private static List<CommonRoomRequest> ReadRequests(IDataReader reader)
@@ -185,17 +204,7 @@ public class CommonRoomRequestDbRepository : BaseDbRepository, ICommonRoomReques
 
     private static CommonRoomRequest ReadRequest(IDataReader reader)
     {
-        User tenant = UserMapper.MapWithAliases(
-            reader,
-            new UserColumnAliases(
-                "user_id",
-                "tenant_username",
-                "tenant_password",
-                "tenant_name",
-                "tenant_surname",
-                "tenant_birthday",
-                "tenant_role"));
-
+        User tenant = UserMapper.MapWithAliases(reader, TenantAliases);
         return CommonRoomRequestMapper.Map(reader, tenant);
     }
 }
