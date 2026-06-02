2272cae1336fc931bf22a6b9699d253faeb4f0e8
Katarina Ostojic <ostojickatarinaaaa@gmail.com>
2026-06-02 12:58:09 +0200

    add resident meetings dto classes

---
+++ src/CommunityHub.Application/DTOs/Buildings/ResidentMeetings/CreateResidentMeetingTopicSuggestionDto.cs.cs
@@ -0,0 +1,18 @@
+﻿namespace CommunityHub.Application.DTOs.Buildings.ResidentMeetings;
+
+public class CreateResidentMeetingTopicSuggestionDto
+{
+    public long MeetingId { get; init; }
+    public long TenantId { get; init; }
+    public string Topic { get; init; }
+
+    public CreateResidentMeetingTopicSuggestionDto(
+        long meetingId,
+        long tenantId,
+        string topic)
+    {
+        MeetingId = meetingId;
+        TenantId = tenantId;
+        Topic = topic;
+    }
+}
